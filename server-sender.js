42dedb8ac24b0059043cdc417fc3a462dbdd0212
andreinwald <andreinwald@gmail.com>
2023-10-14 21:46:05 +0200

    readme

--- server-sender.js
+++ server-sender.js
@@ -4,3 +4,5 @@
 // Newer share your PRIVATE_VAPID_KEY. It should be stored in a safe storage
 const VAPID_PUBLIC_KEY = "BAwUJxIa7mJZMqu78Tfy2Sb1BWnYiAatFCe1cxpnM-hxNtXjAwaNKz1QKLU8IYYhjUASOFzSvSnMgC00vfsU0IM"
 const VAPID_PRIVATE_KEY = "wH3Mire4Nrw7UWiYuD4e76qSezQ-5Nf_9n_HK3vHBSU";
+
+
--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,13 @@
 self.addEventListener('push', (event) => {
+    // PushData sample - https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+    // {
+    //   "title": "Push title",
+    //   "body": "Additional text with some description",
+    //   "data": {
+    //     "url": "https://andreinwald.github.io/webpush-ios-example/success.html"
+    //   }
+    // }
     let pushData = event.data.json();
-    // pushData sample: {"title": "Push title","body":"body", "url":"https://andreinwald.github.io/webpush-ios-example/success.html"}
-    // read more about fields https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
     if (!pushData || !pushData.title) {
         console.error('Received WebPush with an empty title. Received body: ', pushData);
     }
@@ -14,8 +20,12 @@ self.addEventListener('push', (event) => {
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
 
-    if (!event.notification.data || !event.notification.data.url) {
-        console.error('Click on WebPush without url. Received body: ', event.notification)
+    if (!event.notification.data) {
+        console.error('Click on WebPush with empty data, where url should be. Notification: ', event.notification)
+        return;
+    }
+    if (!event.notification.data.url) {
+        console.error('Click on WebPush without url. Notification: ', event.notification)
         return;
     }
 
