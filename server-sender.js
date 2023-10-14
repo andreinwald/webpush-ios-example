dad63bb447c3361dcb33a133331094061d5a4ac8
andreinwald <andreinwald@gmail.com>
2023-10-14 20:35:22 +0200

    created Server for sending script

---
+++ server-sender.js
@@ -0,0 +1,6 @@
+// Public part of VAPID key, generation of that covered in README @TODO
+// All subscription tokens associated with that key, so if you change it - you may lost old subscribers
+// You MUST need generate your own VAPID keys!
+// Newer share your PRIVATE_VAPID_KEY. It should be stored in a safe storage
+const VAPID_PUBLIC_KEY = "BAwUJxIa7mJZMqu78Tfy2Sb1BWnYiAatFCe1cxpnM-hxNtXjAwaNKz1QKLU8IYYhjUASOFzSvSnMgC00vfsU0IM"
+const VAPID_PRIVATE_KEY = "wH3Mire4Nrw7UWiYuD4e76qSezQ-5Nf_9n_HK3vHBSU";
