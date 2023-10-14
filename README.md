8c3b245376a1a05212eb8db8847f2bb7ad1858e9
andreinwald <andreinwald@gmail.com>
2023-10-14 14:49:57 +0200

    sw

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ WebPush for IOS sample code
 
 
 Resources:
-- https://developer.apple.com/videos/play/wwdc2022/10098/
+- [Meet Web Push for Safari on developer.apple.com](https://developer.apple.com/videos/play/wwdc2022/10098/)
 - https://blog.mozilla.org/services/2016/08/23/sending-vapid-identified-webpush-notifications-via-mozillas-push-service/
 - [WebPush PHP sending lib](https://github.com/web-push-libs/web-push-php)
 - [Vapid token](https://blog.mozilla.org/services/2016/08/23/sending-vapid-identified-webpush-notifications-via-mozillas-push-service/)
--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,26 @@
-self.addEventListener("install", (event) => {
-    console.log('ServiceWorker is installed');
+// Code mostly from https://developer.apple.com/videos/play/wwdc2022/10098/
+self.addEventListener('push', (event) => {
+    console.log('Received Push', event.data);
+
+    let pushMessageJSON = event.data.json();
+
+    // Our server puts everything needed to show the notification
+    // in our JSON data.
+    event.waitUntil(self.registration.showNotification(pushMessageJSON.title, {
+        body: pushMessageJSON.body,
+        tag: pushMessageJSON.tag,
+        actions: [{
+            action: pushMessageJSON.actionURL,
+            title: pushMessageJSON.actionTitle,
+        }]
+    }));
 });
 
-self.addEventListener("activate", (event) => {
-    console.log('ServiceWorker is activated');
+self.addEventListener('notificationclick', async function(event) {
+    if (!event.action)
+        return;
+
+    // This always opens a new browser tab,
+    // even if the URL happens to already be open in a tab.
+    clients.openWindow(event.action);
 });
