963c9b00707cda0b6dd21fb1f3734f7c8154ae9c
andreinwald <andreinwald@gmail.com>
2023-10-14 21:27:23 +0200

    sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,11 @@
 // Code based on https://developer.apple.com/videos/play/wwdc2022/10098/
 self.addEventListener('push', (event) => {
     let pushData = event.data.json();
+    // Push data sample: {"title": "Push title","body":"body", "url":"https://andreinwald.github.io/webpush-ios-example/success.html"}
     if (!pushData.title) {
-        console.error('Received WebPush with an empty title. Received body: ' + JSON.stringify(pushData));
+        console.error('Received WebPush with an empty title. Received body: ', pushData);
     }
-
-    event.waitUntil(self.registration.showNotification(pushData.title, {
-        body: pushData.body,
-        tag: pushData.tag,
-        // actions: [{
-        //     action: pushData.actionURL,
-        //     title: pushData.actionTitle,
-        // }]
-    }));
+    event.waitUntil(self.registration.showNotification(pushData.title, pushData));
 });
 
 
@@ -21,15 +14,15 @@ self.addEventListener('notificationclick', function (event) {
 
     console.log(event);
 
-    // if (!event.notification || !event.notification.data || !event.notification.data.url) {
-    //     console.error('Received WebPush without url. Received body: ' + JSON.stringify(event.notification))
-    //     return;
-    // }
-    //
-    // event.waitUntil(
-    //     clients.matchAll({type: 'window'}).then(function () {
-    //         return clients.openWindow(event.notification.data.url)
-    //     })
-    // );
+    if (!event.notification.url) {
+        console.error('Received WebPush without url. Received body: ', event.notification)
+        return;
+    }
+
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then(function () {
+            return clients.openWindow(event.notification.url)
+        })
+    );
     // Track click
 });
