eec63da98c87c134ad4e766244f447e3ae272b21
Andrey Reinwald <andreinwald@gmail.com>
2023-11-05 09:16:58 +0100

    readme Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,5 @@
 self.addEventListener('push', (event) => {
-    // PushData sample - https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
-    // {
-    //   "title": "Push title",
-    //   "body": "Additional text with some description",
-    //   "icon": "https://andreinwald.github.io/webpush-ios-example/images/push_icon.jpg",
-    //   "data": {
-    //     "url": "https://andreinwald.github.io/webpush-ios-example/success.html",
-    //     "message_id": "your_internal_unique_message_id_for_tracking"
-    //   }
-    // }
+    // PushData keys structure standart https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
     let pushData = event.data.json();
     if (!pushData || !pushData.title) {
         console.error('Received WebPush with an empty title. Received body: ', pushData);
@@ -16,7 +7,7 @@ self.addEventListener('push', (event) => {
     self.registration.showNotification(pushData.title, pushData)
         .then(() => {
             // You can save to your analytics fact that push was shown
-            fetch('https://your_backend_server.com/track_show?message_id=' + pushData.data.message_id);
+            // fetch('https://your_backend_server.com/track_show?message_id=' + pushData.data.message_id);
         });
 });
 
@@ -35,6 +26,6 @@ self.addEventListener('notificationclick', function (event) {
     clients.openWindow(event.notification.data.url)
         .then(() => {
             // You can send fetch request to your analytics API fact that push was clicked
-            fetch('https://your_backend_server.com/track_click?message_id=' + pushData.data.message_id);
+            // fetch('https://your_backend_server.com/track_click?message_id=' + pushData.data.message_id);
         });
 });
